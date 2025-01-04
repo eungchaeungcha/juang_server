14cbe8daca266a307a015b1b6f1555e7c28d2d0d
kdmstj <rkddmstj429@gmail.com>
2025-01-04 23:38:25 +0900

    init: 프로젝트 초기 설정
    - 스프링 부트를 활용하여 프로젝트를 초기화
    - 로컬 개발 환경을 위한 H2 인메모리 데이터베이스 설정
    - JPA 및 Spring Web 의존성 추가

---
+++ src/main/java/com/eungchaeungcha/juang/JuangApplication.java
@@ -0,0 +1,13 @@
+package com.eungchaeungcha.juang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eungchaeungcha/juang/JuangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eungchaeungcha.juang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
