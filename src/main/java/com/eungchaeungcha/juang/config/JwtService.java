cdfd2cc8affc1999318d1079a5393b8cba4ecab9
kdmstj <rkddmstj429@gmail.com>
2025-02-24 10:47:46 +0900

    fix: access token 만료 시간 변경
    - 추후에 refresh token 생성 + 만료 시간 수정할 예정입니다.

--- src/main/java/com/eungchaeungcha/juang/config/JwtService.java
+++ src/main/java/com/eungchaeungcha/juang/config/JwtService.java
@@ -50,7 +50,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 365))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
