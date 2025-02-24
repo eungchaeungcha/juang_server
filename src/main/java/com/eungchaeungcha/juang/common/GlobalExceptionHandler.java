5e5fb659628ad5eebda1e9042664d5e20f167897
EunSeo Kang <62414231+kdmstj@users.noreply.github.com>
2025-02-24 11:21:39 +0900

    Revert "20250224 배포"

--- src/main/java/com/eungchaeungcha/juang/common/GlobalExceptionHandler.java
+++ src/main/java/com/eungchaeungcha/juang/common/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -35,34 +34,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleBusinessLogicException(BusinessException e) {
 
         ErrorCode errorCode = e.getErrorCode();
-
-        return ResponseEntity.status(errorCode.getHttpStatus())
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(makeErrorResponse(errorCode));
-    }
-
-
-    private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
-        return ErrorResponse.builder()
-                .code(errorCode.name())
-                .message(errorCode.getMessage())
-                .build();
-    }
-
-    @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<Object> handleBusinessLogicException(BusinessException e) {
-
-        ErrorCode errorCode = e.getErrorCode();
-
-        return ResponseEntity.status(errorCode.getHttpStatus())
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(makeErrorResponse(errorCode));
-    }
-
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<Object> handleAuthenticationException() {
-
-        ErrorCode errorCode = CommonErrorCode.INVALID_USERNAME_AND_PASSWORD;
 
         return ResponseEntity.status(errorCode.getHttpStatus())
                 .contentType(MediaType.APPLICATION_JSON)
