eb1678c08b3c1909bd60e55039040d4b0c8a6848
kdmstj <rkddmstj429@gmail.com>
2025-02-24 10:27:46 +0900

    add: 로그인 예외 처리
    - 아이디가 유효하지 않거나 아이디/비밀번호가 일치하지 않는 경우 AuthenticationException 을 상속받은 BadCredentialException 이 발생하므로 이를 처리하는 Handler 를 구현하였습니다.

--- src/main/java/com/eungchaeungcha/juang/config/ApplicationConfig.java
+++ src/main/java/com/eungchaeungcha/juang/config/ApplicationConfig.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -37,7 +37,7 @@ public class ApplicationConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new BadCredentialsException("invalid username"));
     }
 
     @Bean
